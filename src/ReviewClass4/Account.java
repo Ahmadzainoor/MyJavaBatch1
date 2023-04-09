62d38f07958b85363ddf71672ec1d2807df52377
Ahmadzainoor <=>
2023-04-09 12:53:54 -0400

    my java

---
+++ src/ReviewClass4/Account.java
@@ -0,0 +1,13 @@
+package ReviewClass4;
+
+public class Account {
+
+    public static void main(String[] args) {
+
+        String userNames;
+        String password;
+        double balance;
+        String accountNumber;
+        
+    }
+}
---
+++ src/ReviewClass4/Demo1.java
@@ -0,0 +1,9 @@
+package ReviewClass4;
+
+import sun.applet.Main;
+
+public class Demo1 {
+    public static void main(String[] args) {
+
+    }
+}
